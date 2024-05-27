5302e38f219b2469e80d7eb00543fd4c9b61efcb
Eko450-eng <ekrem@wipdesign.de>
2024-05-27 08:40:38 +0200

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import * as XLSX from "xlsx"
+import * as jsontoxml from "jsontoxml"
+
+// Read the file
+function logSheet() {
+    const workbook = XLSX.readFile("a.xlsx")
+    let worksheets = {}
+    for (const sheetName of workbook.SheetNames) {
+        worksheets[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
+    }
+    console.log(worksheets)
+}
+
+</script>
+
+<template>
+    <button @click="logSheet()">Generate</button>
+</template>
